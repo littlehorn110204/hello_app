158d82d0d589e14658fa72b4a185389c62040681
littlehorn110204 <72970223+littlehorn110204@users.noreply.github.com>
2021-06-10 04:38:20 -0400

    Initialized repository of Ruby on Rails first app commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,7 @@
+class ApplicationController < ActionController::Base
+
+	def Здрaвствуйте
+	render html: "Здрaвствуйте, мир от моего терминала Linux!!!
+			(Zdravstvuyte, mir ot moyego terminala Linux)"
+	end
+end
